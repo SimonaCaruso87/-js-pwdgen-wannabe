7095afae45fcc3ebeb6a57e4340478b741e8dce8
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-09 17:18:54 +0200

    Esercizio completato

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # -js-pwdgen-wannabe
+
+Ciao ragazzi,
+esercizio di oggi: (insicurissimo) password generator
+nome repo: js-pwdgen-wannabe
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito101 (modificato) 
---
+++ js/script.js
@@ -0,0 +1,19 @@
+// QUANDO DIAMO UNA VARIABILE UTILIZZARE UN NOME IDENTIFICATIVO
+    
+        // tra parentesi troviamo il Label che apparirà dentro il pop-up/allert
+
+
+// const è una variabile  , name è il nome della variabile che è obbligatorio 
+
+// prompt crea un allert con un campo riempibile
+
+
+const firstName=prompt("Nome");
+
+const lastName=prompt("Cognome");
+
+const favoriteColor=prompt("Colore preferito");
+
+// console.log( firstName, lastName , favoriteColor);
+
+document.getElementById("pwd").innerHTML=firstName + lastName + favoriteColor + "101"
